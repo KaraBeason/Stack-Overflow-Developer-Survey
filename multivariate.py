cabf0c1bc24f10c904b89d36af949d60176d0add
parkerat2 <parkerat2@appstate.edu>
2018-05-09 23:39:12 -0400

    Add files via upload

--- multivariate.py
+++ multivariate.py
@@ -1,21 +1,17 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-import matplotlib.colors as mcolors
-
 import random
-
 import math
 
-df = pd.read_csv('data/2016 Stack Overflow Survey Responses.csv')
+# df = pd.read_csv('data/2016 Stack Overflow Survey Responses.csv')
 # print(df)
-pd.to_pickle(df, 'data/SOpickle')
+# pd.to_pickle(df, 'data/SOpickle')
 
 df = pd.read_pickle('data/SOpickle')
 # print(list(df))
 country = np.array(df['country'].data)
-soregion = np.array(df['so_region'].data)  # stack overflow region
+soregion = np.array(df['so_region'].data)
 age = np.array(df['age_midpoint'].data)
 gender = np.array(df['gender'].data)
 occupation = np.array(df['occupation'].data)
@@ -28,7 +24,7 @@ industry = np.array(df['industry'].data)
 companysize = np.array(df['company_size_range'].data)
 teamsize = np.array(df['team_size_range'].data)
 women = np.array(df['women_on_team'].data)
-remote = np.array(df['remote'].data)  # how often do you work remotely
+remote = np.array(df['remote'].data)
 jobsatisfaction = np.array(df['job_satisfaction'].data)
 pet = np.array(df['dogs_vs_cats'].data)
 education = np.array(df['education'].data)
@@ -76,7 +72,6 @@ def agreevalue(x):
     #     x == 0.5
     return x
 
-
 def importancevalue(x):
     if x == "I don't care about this":
         x = random.randint(0, 10) / 1000
@@ -116,9 +111,8 @@ def agree():
         for i in range(len(atech)):
             if len(idxlist) < 100:
                 if type(atech[i]) is str and type(anotice[i]) is str and type(aproblem[i]) is str and \
-                        type(adiversity[i]) is str and type(aadblocker[i]) is str and type(aalcohol[i]) is str and \
-                        type(aboss[i]) is str and type(anight[i]) is str and type(alegacy[i]) is str and type(
-                    amars[i]) is str:
+                        type(adiversity[i]) is str and type(aadblocker[i]) is str and type (aalcohol[i]) is str and \
+                        type(aboss[i]) is str and type(anight[i]) is str and type(alegacy[i]) is str and type(amars[i]) is str:
                     idxlist.append(indx)
                     atech[i] = agreevalue(atech[i])
                     anotice[i] = agreevalue(anotice[i])
@@ -235,6 +229,7 @@ def agree():
     plt.text(-1, 0.50, s='Neutral', va='top', ha='center')
     plt.text(-1, 0.76, s='Agree S', va='top', ha='center')
     plt.text(-1, 1, s='Agree C', va='top', ha='center')
+
     plt.text(0, 0, s='work tech', va='top', ha='center')
     plt.text(1, 1, s='notice improvements', va='bottom', ha='center')
     plt.text(2, 0, s='problem solving', va='top', ha='center')
@@ -262,164 +257,10 @@ def agree():
 
     plt.title("Agree or Disagree Region Averages")
     plt.legend(loc=(0.06, 0.06))
-    plt.savefig('/imgs/AoD_avgs.png')
+    plt.savefig('imgs/AoD_avgs.png')
     plt.show()
     plt.clf()
 
-#  CUSTOM COLOR MAP that looks cool but obfuscates the message of the data.
-# def make_colormap(seq):
-#     """Return a LinearSegmentedColormap
-#     seq: a sequence of floats and RGB-tuples. The floats should be increasing
-#     and in the interval (0,1).
-#     """
-#     seq = [(None,) * 3, 0.0] + list(seq) + [1.0, (None,) * 3]
-#     cdict = {'red': [], 'green': [], 'blue': []}
-#     for i, item in enumerate(seq):
-#         if isinstance(item, float):
-#             r1, g1, b1 = seq[i - 1]
-#             r2, g2, b2 = seq[i + 1]
-#             cdict['red'].append([item, r1, r2])
-#             cdict['green'].append([item, g1, g2])
-#             cdict['blue'].append([item, b1, b2])
-#
-#     return mcolors.LinearSegmentedColormap('CustomMap', cdict)
-#
-#
-# c = mcolors.ColorConverter().to_rgb
-# rvb_f = make_colormap(
-#     [c('indigo'), 0.125, c('indigo'), c('darkmagenta'), 0.25, c('darkmagenta'), c('orchid'), 0.5, c('orchid'),
-#      0.7, c('hotpink'), c('hotpink'), 0.75, c('lightpink')])
-# rvb_m = make_colormap(
-#     [c('midnightblue'), 0.125, c('midnightblue'), c('teal'), 0.25, c('teal'), c('mediumturquoise'), 0.5, c('mediumspringgreen'),
-#      0.7, c('mediumspringgreen'), c('skyblue'), 0.75, c('skyblue')])
-
-def gender_salary():
-    ageset = set(age)
-    ageset = sorted([x for x in ageset if x == x])
-    # print(salaryset)
-    # df["gender"] =
-    no_nan_df = df.dropna(subset=['gender', 'salary_midpoint', 'age_midpoint', 'experience_midpoint', 'industry'])
-    no_nan_df = no_nan_df[no_nan_df.gender != 'Prefer not to disclose']
-    no_nan_df = no_nan_df[no_nan_df.gender != 'Other']
-    female_salary_by_age = list(range(8))
-    female_count_age = list(range(8))
-    male_salary_by_age = list(range(8))
-    male_count_age = list(range(8))
-    male_experience_by_age = list(range(8))
-    female_experience_by_age = list(range(8))
-
-    industries_set = no_nan_df.industry.values
-    female_industry_by_age = dict.fromkeys(set(industries_set), 0)
-    male_industry_by_age = dict.fromkeys(set(industries_set), 0)
-    female_industry_salary = dict.fromkeys(set(industries_set), 0)
-    male_industry_salary = dict.fromkeys(set(industries_set), 0)
-    # iterate through data and keep track of age, gender, counts to get average salary and average experience
-    #   by age group and gender
-    for index, rows in no_nan_df.iterrows():
-        if rows['age_midpoint'] == 16:
-            insert_i = 0
-        if rows['age_midpoint'] == 22:
-            insert_i = 1
-        if rows['age_midpoint'] == 27:
-            insert_i = 2
-        if rows['age_midpoint'] == 32:
-            insert_i = 3
-        if rows['age_midpoint'] == 37:
-            insert_i = 4
-        if rows['age_midpoint'] == 44.5:
-            insert_i = 5
-        if rows['age_midpoint'] == 54.5:
-            insert_i = 6
-        if rows['age_midpoint'] == 65:
-            insert_i = 7
-        if rows['gender'] == 'Female':
-            female_count_age[insert_i] += 1
-            female_salary_by_age[insert_i] += rows['salary_midpoint']
-            female_experience_by_age[insert_i] += rows['experience_midpoint']
-            female_industry_by_age[rows['industry']] += 1
-            female_industry_salary[rows['industry']] += rows['salary_midpoint']
-        elif rows['gender'] == 'Male':
-            male_count_age[insert_i] += 1
-            male_salary_by_age[insert_i] += rows['salary_midpoint']
-            male_experience_by_age[insert_i] += rows['experience_midpoint']
-            male_industry_by_age[rows['industry']] += 1
-            male_industry_salary[rows['industry']] += rows['salary_midpoint']
-
-    average_male_salary = list(range(8))
-    average_female_salary = list(range(8))
-    average_male_experience = list(range(8))
-    average_female_experience = list(range(8))
-
-    for i in range(0, 8, 1):
-        average_female_experience[i] = female_experience_by_age[i] / female_count_age[i]
-        average_female_salary[i] = female_salary_by_age[i] / female_count_age[i]
-        average_male_experience[i] = male_experience_by_age[i] / male_count_age[i]
-        average_male_salary[i] = male_salary_by_age[i] / male_count_age[i]
-    plt.figure(0)
-    ax = plt.subplot(111)
-    ax2 = ax.twinx()
-    x = [16, 22, 27, 32, 37, 44.5, 54.5, 65]
-    for i in range(0, 8, 1):
-        ax.bar(x[i] - 1.5, average_female_salary[i], color='hotpink')
-        ax.bar(x[i] - .5, average_male_salary[i], color='mediumturquoise')
-        ax2.bar(x[i] + .5, average_female_experience[i], color='darkmagenta')
-        ax2.bar(x[i] + 1.5, average_male_experience[i], color='teal')
-    ax.set_xlabel("Age Midpoint")
-    ax.set_ylabel("Average Salary in USD")
-    ax2.set_ylabel("Average Experience")
-    handles = []
-    fem_sal = mpatches.Patch(color='hotpink', label='Female Salary')
-    handles.append(fem_sal)
-    male_sal = mpatches.Patch(color='mediumturquoise', label='Male Salary')
-    handles.append(male_sal)
-    fem_exp = mpatches.Patch(color='darkmagenta', label='Female Experience')
-    handles.append(fem_exp)
-    male_exp = mpatches.Patch(color='teal', label='Male Experience')
-    handles.append(male_exp)
-    plt.legend(handles=handles)
-    plt.xticks(ageset)
-    plt.tight_layout()
-    plt.savefig("Average Salary and Experience by Gender and Age Group.png")
-
-    avg_female_salary_industry = dict(
-        (k, float(female_industry_salary[k]) / female_industry_by_age[k]) for k in female_industry_salary)
-    avg_male_salary_industry = dict(
-        (k, float(male_industry_salary[k]) / male_industry_by_age[k]) for k in male_industry_salary)
-
-    fig2 = plt.figure(1)
-    ax4 = plt.subplot(111)
-    ax3 = ax4.twinx()
-    print(female_industry_by_age.values())
-    x = np.arange(0, len(female_industry_by_age), 1)
-    x = np.multiply(x, 5)
-    N = len(female_industry_by_age)
-    ax4.set_xlabel("Industry")
-    ax4.set_ylabel("Average Salary in USD")
-    ax3.set_ylabel("Number of Participants")
-    ax3.bar(x - 1.25, list(female_industry_by_age.values()), align='center', width=.5, color= 'violet')
-    ax4.bar(x - .5, list(avg_female_salary_industry.values()), align='center', width=.5, color='deeppink')
-    ax3.bar(x + .5, list(male_industry_by_age.values()), align='center', width=.5, color='c')
-    ax4.bar(x + 1.25, list(avg_male_salary_industry.values()), align='center', width=.5, color='darkcyan')
-    # plt.tight_layout()
-
-    ax4.set_xticks(x)
-    ax4.set_xticklabels(list(female_industry_by_age.keys()), rotation='vertical')
-    # plt.savefig("Average Salary and Number of Participants by Industry.png")
-    handles = []
-    fem_ind = mpatches.Patch(color='violet', label='Fem Industry')
-    handles.append(fem_ind)
-    male_ind = mpatches.Patch(color='c', label='Male Industry')
-    handles.append(male_ind)
-    fem_exp = mpatches.Patch(color='deeppink', label='Female Salary')
-    handles.append(fem_exp)
-    male_exp = mpatches.Patch(color='darkcyan', label='Male Salary')
-    handles.append(male_exp)
-    plt.legend(handles=handles)
-    plt.tight_layout()
-    # plt.axis('equal')
-    plt.show()
-
-    # plt.savefig("Average Salary and Participation by Industry.png")
 
 def important():
     # choices = ['I don't care about this', 'This is somewhat important', 'This is very important']
@@ -450,9 +291,8 @@ def important():
         for i in range(len(atech)):
             if len(idxlist) < 100:
                 if type(ivariety[i]) is str and type(icontrol[i]) is str and type(isameend[i]) is str and \
-                        type(inewtech[i]) is str and type(ibuildnew[i]) is str and type(ibuildexisting[i]) is str and \
-                        type(ipromotion[i]) is str and type(imission[i]) is str and type(iwfh[i]) is str and type(
-                    ioffice[i]) is str:
+                        type(inewtech[i]) is str and type(ibuildnew[i]) is str and type (ibuildexisting[i]) is str and \
+                        type(ipromotion[i]) is str and type(imission[i]) is str and type(iwfh[i]) is str and type(ioffice[i]) is str:
                     idxlist.append(indx)
                     ivariety[i] = importancevalue(ivariety[i])
                     icontrol[i] = importancevalue(icontrol[i])
@@ -513,9 +353,8 @@ def important():
             plt.plot([7, 8], [imissionavg, iwfhavg], color='r', lw=2)
             plt.plot([8, 9], [iwfhavg, iofficeavh], color='r', lw=2)
 
-            avglist.append(
-                [ivarietyavg, icontrolavg, isameendavg, inewtechavg, ibuildnewavg, ibuildexistingavg, ipromotionavg,
-                 imissionavg, iwfhavg, iofficeavh])
+            avglist.append([ivarietyavg, icontrolavg, isameendavg, inewtechavg, ibuildnewavg, ibuildexistingavg, ipromotionavg,
+                            imissionavg, iwfhavg, iofficeavh])
 
             plt.text(-1, 0.02, s="Don't Care", va='top', ha='center')
             plt.text(-1, 0.50, s='Somewhat Important', va='top', ha='center')
@@ -547,7 +386,7 @@ def important():
             ax.axis('off')
 
             plt.title("Importance " + region + " Region")
-            plt.savefig('imgs/I' + region + ".png")
+            plt.savefig('imgs/I'+ region + ".png")
             plt.show()
             plt.clf()
 
@@ -599,11 +438,11 @@ def important():
     plt.clf()
 
 
+
 def main():
-    gender_salary()
     # agree()
     # important()
-    # print("hello")
+    print("hello")
     # setvar = set(atech)
     # setvar = sorted([x for x in setvar if x == x])
     # print(setvar)
@@ -611,3 +450,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
--- text.py
+++ text.py
@@ -4,6 +4,9 @@ import nltk as nl
 import nltk.corpus as nc
 import re as regex
 import math
+import operator
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def removelinks(csvpath):
@@ -18,10 +21,14 @@ def removelinks(csvpath):
     csv['message'] = mes
     csv.to_csv("data/linksremoved.csv", index=False)
 
+
 def removestop(csvpath, stop):
     csv = pd.read_csv(csvpath)
     mes = csv['message']
-
+    cl = csv['Coding:Level1']
+    cidx = []
+    iidx = []
+    aidx = []
     for i in range(len(mes)):
     # for i in range(1):
         if not isinstance(mes[i], str):
@@ -31,44 +38,176 @@ def removestop(csvpath, stop):
         m = nl.tokenize.word_tokenize(mes[i])
         sentence = []
         for w in m:
+            w = w.lower()
             if w not in stop:
                 sentence.append(w)
         mes[i] = sentence
+        if cl[i] == 'Information':
+            iidx.append(i)
+        elif cl[i] == 'Community':
+            cidx.append(i)
+        elif cl[i] == 'Action':
+            aidx.append(i)
         # print(sentence)
 
-    csv['message'] = mes
+    for i in range(len(cl)):
+        csv['message'] = mes
     csv.to_csv("data/stopsremoved.csv", index=False)
 
+    return mes, cl, cidx, iidx, aidx
+
+
+def counts(idxlist, messages):
+    countdict = {}
+    for i in idxlist:
+        for word in messages[i]:
+            if word in countdict:
+                countdict[word] += 1
+            else:
+                countdict[word] = 1
+    sortedcounts = sorted(countdict.items(), key=operator.itemgetter(1), reverse=True)
+
+    return sortedcounts[0:50]
 
 def main():
     # removelinks('data/labelsmessages.csv')
 
-    # stop = nc.stopwords
-    # s = set(stop.words('english'))
-    # s.add('!')
-    # s.add('.')
-    # s.add('?')
-    # s.add('\n')
-    # s.add('\\n')
-    # s.add('\\n\\n')
-    # s.add('@')
-    # s.add('\'')
-    # s.add(',')
-    # s.add(';')
-    # s.add(':')
-    # s.add('(')
-    # s.add(')')
-    # s.add('#')
-    # s.add('...')
-    # s.add("'s")
-    # s.add("n't")
-    # print(s)
-    # removestop('data/linksremoved.csv', s)
+    stop = nc.stopwords
+    s = set(stop.words('english'))
+    s.add('!')
+    s.add('.')
+    s.add('?')
+    s.add('\n')
+    s.add('\\n')
+    s.add('\\n\\n')
+    s.add('\\n\\nwe')
+    s.add('\\nwe')
+    s.add('@')
+    s.add('\'')
+    s.add(',')
+    s.add(';')
+    s.add(':')
+    s.add('(')
+    s.add(')')
+    s.add('#')
+    s.add('...')
+    s.add("'s")
+    s.add("n't")
+    s.add('``')
+    s.add("''")
+    s.add("'re")
+    s.add('de')
+    mes, cl, cidx, iidx, aidx = removestop('data/linksremoved.csv', s)
 
-    csv = pd.read_csv('data/stopsremoved.csv')
-    mes = csv['message']
+    iwords = counts(iidx, mes)
+    awords = counts(aidx, mes)
+    cwords = counts(cidx, mes)
+    l = [i for i in range(len(mes))]
+    entire = counts(l, mes)
+
+    fontscale = iwords[0][1] + iwords[-1][1]
+    iwords = dict(iwords)
+
+    fonts = []
+    icolors = []
+    idict = {}
+    for key, value in zip(iwords.keys(), iwords.values()):
+        fonts.append((key, 40 * value / fontscale))
+        icolors.append(value / fontscale)
+        idict[key] = value / fontscale
+    xs = [x for x in np.arange(1/8, 1, 1/4)]
+    ys = [y for y in np.arange(9/10, 0, -1/10)]
+    for i in range(len(xs)):
+        for j in range(len(ys)):
+            plt.text(xs[i], ys[j], s=fonts[i * len(ys) + j][0], fontsize=fonts[i * len(ys) + j][1], ha='center', va='center',
+                     color=(icolors[i * len(ys) + j], 0, 0))
+    ax = plt.gca()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    plt.title("Information Most Popular Words")
+    plt.savefig('imgs/I_words.png')
+    plt.show()
+    plt.clf()
+
+    fontscale = awords[0][1] + awords[-1][1]
+    awords = dict(awords)
+
+    fonts = []
+    acolors = []
+    adict = {}
+    for key, value in zip(awords.keys(), awords.values()):
+        fonts.append((key, 40 * value / fontscale))
+        acolors.append(value / fontscale)
+        adict[key] = value / fontscale
+    xs = [x for x in np.arange(1/8, 1, 1/4)]
+    ys = [y for y in np.arange(9/10, 0, -1/10)]
+    for i in range(len(xs)):
+        for j in range(len(ys)):
+            plt.text(xs[i], ys[j], s=fonts[i * len(ys) + j][0], fontsize=fonts[i * len(ys) + j][1], ha='center', va='center',
+                     color=(0, acolors[i * len(ys) + j], 0))
+    ax = plt.gca()
+    plt.title("Action Most Popular Words")
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    plt.savefig('imgs/A_words.png')
+    plt.show()
+    plt.clf()
+
+    fontscale = cwords[0][1] + cwords[-1][1]
+    cwords = dict(cwords)
+
+    fonts = []
+    ccolors = []
+    cdict = {}
+    for key, value in zip(cwords.keys(), cwords.values()):
+        fonts.append((key, 40 * value / fontscale))
+        ccolors.append(value / fontscale)
+        cdict[key] = value / fontscale
+    xs = [x for x in np.arange(1/8, 1, 1/4)]
+    ys = [y for y in np.arange(9/10, 0, -1/10)]
+    for i in range(len(xs)):
+        for j in range(len(ys)):
+            plt.text(xs[i], ys[j], s=fonts[i * len(ys) + j][0], fontsize=fonts[i * len(ys) + j][1], ha='center', va='center',
+                     color=(0, 0, ccolors[i * len(ys) + j]))
+    ax = plt.gca()
+    plt.title("Community Most Popular Words")
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    plt.savefig('imgs/C_words.png')
+    plt.show()
+    plt.clf()
+
+    fontscale = entire[0][1] + entire[-1][1]
+    entire = dict(entire)
+
+    fonts = []
+    ecolors = []
+    for key, value in zip(entire.keys(), entire.values()):
+        fonts.append((key, 40 * value / fontscale))
+        r = 0
+        g = 0
+        b = 0
+        if key in idict:
+            r = idict[key]
+        if key in adict:
+            g = adict[key]
+        if key in cdict:
+            b = cdict[key]
+        ecolors.append((r, g, b))
+    xs = [x for x in np.arange(1/8, 1, 1/4)]
+    ys = [y for y in np.arange(9/10, 0, -1/10)]
+    for i in range(len(xs)):
+        for j in range(len(ys)):
+            plt.text(xs[i], ys[j], s=fonts[i * len(ys) + j][0], fontsize=fonts[i * len(ys) + j][1], ha='center', va='center',
+                     color=(ecolors[i * len(ys) + j]))
+    ax = plt.gca()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    plt.title("Most Popular Words")
+    plt.savefig('imgs/E_words.png')
+    plt.show()
+    plt.clf()
 
 
 if __name__ == '__main__':
     main()
-
